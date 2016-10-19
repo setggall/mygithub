28e7be7f333bd523dc10a589ab558177dc79a816
Seth Gallimore <trips66@gmail.com>
2016-10-18 22:32:51 -0500

    Simple Github Search App
    Simple Github Search built with React and Webpack

---
+++ src/components/App.jsx
@@ -0,0 +1,85 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Profile from './github/Profile.jsx';
+import Search from './github/Search.jsx';
+
+class App extends Component{
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: 'setggall',
+			userData: [],
+			userRepos: [],
+			perPage: 10
+		}
+	}
+
+	//Get user data from github
+	getUserData() {
+		$.ajax({
+			url: 'https://api.github.com/users/'+this.state.username+'?client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({userData: data});
+
+		}.bind(this),
+			   error: function(xhr, status, err) {
+					 this.setState({username: null});
+					alert(err);
+		}.bind(this)
+		});
+	}
+
+	//Get user repos from github
+	getUserRepos() {
+		$.ajax({
+			url: 'https://api.github.com/users/'+this.state.username+'/repos?per_page='+this.state.perPage+'&client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret+'&sort=created',
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({userRepos: data});
+
+		}.bind(this),
+				 error: function(xhr, status, err) {
+					 this.setState({username: null});
+					alert(err);
+		}.bind(this)
+		});
+	}
+
+handleFormSubmit(username) {
+	this.setState({username: username}, function() {
+		this.getUserData();
+		this.getUserRepos();
+	});
+}
+
+	componentDidMount() {
+		this.getUserData();
+		this.getUserRepos();
+	}
+
+	render(){
+		return(
+			<div>
+				<Search onFormSubmit={this.handleFormSubmit.bind(this)} />
+				<Profile {...this.state} />
+			</div>
+		)
+	}
+}
+
+App.propTypes = {
+	clientId: React.PropTypes.string,
+	clientSecret: React.PropTypes.string
+};
+
+App.defaultProps = {
+	clientId: '9c7602419b87c14351b9',
+	clientSecret: '528aa10f4f44944694512200fccf15c58bfa873d'
+}
+
+
+export default App
